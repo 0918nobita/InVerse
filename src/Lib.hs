5e4c87775e4aa89ecd7e4eda4bc91a469a1ce9cf
0918nobita <nobita.0918@gmail.com>
2018-03-19 23:55:09 +0900

    48 hours scheme 練習問題1.2

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,4 +7,10 @@ import System.Environment
 someFunc :: IO ()
 someFunc = do
     args <- getArgs
-    putStrLn ( "Hello, " ++ args !! 0 ++ " " ++ args !! 1 )
+    putStrLn $ "Input Numbers: " ++ args !! 0 ++ ", " ++ args !! 1
+    let a = read $ args !! 0
+        b = read $ args !! 1
+    putStrLn $ "Sum: " ++ (show $ a + b)
+    putStrLn $ "Difference: " ++ (show $ a - b)
+    putStrLn $ "Product: " ++ (show $ a * b)
+    putStrLn $ "Quotient: " ++ (show $ a / b)
