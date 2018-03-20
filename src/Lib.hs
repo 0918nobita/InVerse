a9ec2bac6a2b033ef6ce27858206ec3b45d2a8c1
0918nobita <nobita.0918@gmail.com>
2018-03-20 18:12:35 +0900

    シンボルのパーサを追加した

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,26 @@ module Lib
     ( someFunc
     ) where
 
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+-- Parser モナド ( 隠蔽されている「追加情報」は入力ストリーム、バックトラックの記録、first と follow 集合など )
+-- oneOf ::  Stream s m Char => [Char] -> ParsecT s u m Char
+--     引数で与えられた文字列中のどれか一文字を認識する ( パースされた文字を返す )
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+-- Parsec は letter や digit など、いくつものこまごまとした既成のパーサを提供する
+
+-- symbol パーサを呼び出し、エラーが発生したらそれを処理する関数
+-- parse は、パースした結果の値、またはエラーを表す Either データ型を返す
+--     Left: エラー, Right: 通常の値
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
 someFunc :: IO ()
 someFunc = do
-    name <- getLine
-    putStrLn name
+    args <- getArgs
+    putStrLn $ readExpr $ args !! 0
